263eb5e5a722258bce5f617b03cf6b6c0e278625
Bill Dirks <bill@greatexpectations.io>
2024-10-25 11:58:44 -0700

    [RELEASE] 0.18.22 (#10574)

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -243,7 +243,7 @@ module.exports = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '0.18.21',
+              label: '0.18.22',
               path: ''
             }
           }
